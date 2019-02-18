1857226da32ee1af36aab654a293f4bc358746e3
Sol Borjas <soltborjas@gmail.com>
2019-02-18 14:58:25 -0500

    I can't believe ugly mie gets uglier when I wor on master branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return(
-    <div>
-      <Link to="/">Home</Link><Link to="/type">Search By Type</Link>
-      <Link to="/name">Search By Name</Link>
+    <div class="links">
+      <Link to="/">Home</Link>  |  <Link to="/type">Search By Type</Link>
+      | <Link to="/name">Search By Name</Link> | <Link to="/move/:move">Search Moves</Link>
     </div>
   )
 }
