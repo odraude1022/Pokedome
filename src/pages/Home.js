8b4235ff16987008d7510bd3db69200841612c9b
Eduardo Iglesias <eriglesias@ufl.edu>
2019-05-06 15:50:20 -0400

    fixed grammatical problems in the home page descriptions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
       <div className="info">
         <div id="left">
           <p>
-            Are you new to Pokemon?! This is the website for you! Here at Poke
-            Dome we offer Pokemon information by filtering them by Name, type
+            Are you new to Pokemon?! This is the website for you! Here at PokeDome,
+            we offer Pokemon information by filtering them by Name, type,
             and moves!
           </p>
         </div>
@@ -22,12 +22,12 @@ const Home = () => {
           <p>Did you know?</p>
           <img id="unknown" src="/images/unknown.gif" alt="unknown" />
           <p>
-            The Pokemon franchise is the HIGHEST-Grossing entertainment
-            franchise of all time!!! Since it’s creation in 1996 Pokemon is
-            estimated to be worth $59.1 billion! Pokemon beats the Star Wars
-            franchise that falls short grossing $49.5 billion! Pokemon Go! came
-            out on July 2016 and had users walk around areas to creating the
-            illustion That users were able to catch their own pokemon!
+            The Pokemon franchise is the Highest-Grossing entertainment
+            franchise of all time! Since its creation in 1996, Pokemon is
+            estimated to have earned more $90 billion! Pokemon beats the Star Wars
+            franchise that falls short grossing $65 billion! Pokemon Go! came
+            out on July 2016 and had users walk around areas to create the
+            illustion that users were able to catch their own pokemon!
           </p>
         </div>
       </div>
