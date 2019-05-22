c013b277929e61e5a463fdef64daf4cbd09a85a1
Eduardo Iglesias <eriglesias@ufl.edu>
2019-05-22 15:05:11 -0400

    make Button component to be returned by the map function in Buttons

--- src/components/Type/Button.js
+++ src/components/Type/Button.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Button = props => {
+  let type = props.type
   return(
-    <div>
-    </div>
+    <Link to={`/type/${type}`}>
+      <img
+        className="type_icon"
+        src={`/images/types/${type}_icon.jpg`}
+        onClick={() => props.handleParameter(type)}
+        alt={type}
+      />
+    </Link>
   )
 }
 
--- src/components/Type/Buttons.js
+++ src/components/Type/Buttons.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Button from "./Button"
-import { Link } from "react-router-dom";
 
 let types=["normal", "bug", "dark", "dragon", "electric", "fairy", "fighting",
        "fire", "flying", "ghost", "grass", "ground", "ice", "poison", "psychic",
@@ -9,18 +8,10 @@ let types=["normal", "bug", "dark", "dragon", "electric", "fairy", "fighting",
 const Buttons = props => {
   return(
     <div id="type_icons">
-    {types.map( type => {
-      return(
-        <Link to={`/type/${type}`}>
-          <img
-            className="type_icon"
-            src={`/images/types/${type}_icon.jpg`}
-            onClick={() => props.handleParameter(type)}
-            alt={type}
-          />
-        </Link>
-      )
-    })}
+      {types.map( type => (
+          <Button type={type} handleParameter={props.handleParameter} />
+      ))
+      }
     </div>
   )
 }
